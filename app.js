ad6bcaebf2eb05e2c5a6aec3ca1beb6cfaf3651f
Derek Kramer <derek.kramer88@gmail.com>
2017-06-08 11:55:56 -0600

    🔎 Added body parser

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const request = require('request');
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded(
+	{
+		extended: false
+	}
+));
 
 app.get('/', (req, res) => {
 	console.log(req.body);
